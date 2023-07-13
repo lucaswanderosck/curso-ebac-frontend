6b92ed9846ed6321f58605576cdf66bf683ef743
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-07-13 14:37:41 -0300

    exercício typescript

---
+++ main.ts
@@ -0,0 +1,11 @@
+const multiply = (numberOne: number, numberTwo: number): number => {
+  return numberOne * numberTwo;
+};
+
+console.log(multiply(45, 14));
+
+const greeting = (userName: string): string => {
+  return `Olá ${userName}`;
+};
+
+console.log(greeting("lucas"));
